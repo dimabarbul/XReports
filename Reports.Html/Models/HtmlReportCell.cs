607ce14f84dd11bbe9e5750a5ec751fa7e18cc3b
Dmitriy Barbul <dimabarbul@gmail.com>
2020-11-11 09:35:31 +0200

    Add more properties to HTML cell

--- Reports.Html/Models/HtmlReportCell.cs
+++ Reports.Html/Models/HtmlReportCell.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Reports.Models;
 
 namespace Reports.Html.Models
@@ -5,6 +6,13 @@ namespace Reports.Html.Models
     public class HtmlReportCell : BaseReportCell
     {
         public string Html { get; set; }
+
+        #region Most used or complex HTML attributes
+        public HashSet<string> CssClasses { get; set; } = new HashSet<string>();
+        public Dictionary<string, string> Styles { get; set; }
+        #endregion
+
+        public Dictionary<string, string> Attributes { get; set; }
 
         public override void CopyFrom(BaseReportCell reportCell)
         {
