13b9189b1326de0bd98d4e2043e56b7b6c59d4a1
Dmitriy Barbul <dimabarbul@gmail.com>
2020-11-29 09:54:54 +0200

    Clean up HomeController

--- demos/Reports.Demos.FromDb/Controllers/HomeController.cs
+++ demos/Reports.Demos.FromDb/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Reports.Demos.FromDb.ViewModels;
@@ -11,11 +7,11 @@ namespace Reports.Demos.FromDb.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<HomeController> logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
-            _logger = logger;
+            this.logger = logger;
         }
 
         public IActionResult Index()
