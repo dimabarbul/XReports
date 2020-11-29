8537b654bc0c35275e0d40f50fce9c36f3939c72
Dmitriy Barbul <dimabarbul@gmail.com>
2020-11-29 08:26:44 +0200

    Add building of horizontal report to EntityAttributeBuilderHelper

--- src/Reports.Extensions.Builders/BuilderHelpers/EntityAttributeBuilderHelper.cs
+++ src/Reports.Extensions.Builders/BuilderHelpers/EntityAttributeBuilderHelper.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using Reports.Extensions.Builders.Attributes;
+using Reports.Extensions.Builders.Enums;
 using Reports.Interfaces;
 using Reports.ReportCellsProviders;
 using Reports.SchemaBuilders;
@@ -12,25 +13,67 @@ namespace Reports.Extensions.Builders.BuilderHelpers
 {
     public class EntityAttributeBuilderHelper
     {
-        public void BuildVerticalReport<TEntity>(VerticalReportSchemaBuilder<TEntity> builder)
+        public IReportSchema<TEntity> BuildSchema<TEntity>()
         {
-            ReportVariableData[] properties = typeof(TEntity).GetProperties()
-                .Select(p => new
-                {
-                    Property = p,
-                    Attribute = p.GetCustomAttribute<ReportVariableAttribute>(),
-                })
-                .Where(x => x.Attribute != null)
-                .Select(x => new ReportVariableData()
-                {
-                    Property = x.Property,
-                    Attribute = x.Attribute,
-                })
-                .OrderBy(x => x.Attribute.Order)
-                .ToArray();
+            ReportTypeAttribute reportTypeAttribute = typeof(TEntity).GetCustomAttribute<ReportTypeAttribute>();
+
+            return reportTypeAttribute?.Type == ReportType.Horizontal
+                ? (IReportSchema<TEntity>) this.BuildVerticalReport<TEntity>().BuildSchema()
+                : (IReportSchema<TEntity>) this.BuildHorizontalReport<TEntity>().BuildSchema();
+        }
+
+        private HorizontalReportSchemaBuilder<TEntity> BuildHorizontalReport<TEntity>()
+        {
+            HorizontalReportSchemaBuilder<TEntity> builder = new HorizontalReportSchemaBuilder<TEntity>();
+
+            ReportVariableData[] reportVariables = this.GetProperties<TEntity>();
+
+            this.AddRows(builder, reportVariables);
+
+            return builder;
+        }
+
+        private void AddRows<TEntity>(HorizontalReportSchemaBuilder<TEntity> builder, ReportVariableData[] reportVariables)
+        {
+            foreach (ReportVariableData x in reportVariables)
+            {
+                this.AddRow(builder, x.Property, x.Attribute);
+            }
+        }
+
+        private void AddRow<TEntity>(HorizontalReportSchemaBuilder<TEntity> builder, PropertyInfo property, ReportVariableAttribute attribute)
+        {
+            IReportCellsProvider<TEntity> instance = this.CreateCellsProvider<TEntity>(property, attribute);
+
+            builder.AddRow(instance);
+
+            this.ApplyAttribute(builder, attribute);
+        }
+
+        private IReportCellsProvider<TEntity> CreateCellsProvider<TEntity>(PropertyInfo property, ReportVariableAttribute attribute)
+        {
+            ParameterExpression parameter = Expression.Parameter(typeof(TEntity), "x");
+            MemberExpression memberExpression = Expression.Property(parameter, typeof(TEntity), property.Name);
+            LambdaExpression lambdaExpression = Expression.Lambda(memberExpression, parameter);
+
+            IReportCellsProvider<TEntity> instance = (IReportCellsProvider<TEntity>) Activator.CreateInstance(
+                typeof(EntityPropertyReportCellsProvider<,>)
+                    .MakeGenericType(typeof(TEntity), property.PropertyType),
+                attribute.Title,
+                lambdaExpression.Compile()
+            );
+            return instance;
+        }
+
+        public VerticalReportSchemaBuilder<TEntity> BuildVerticalReport<TEntity>()
+        {
+            VerticalReportSchemaBuilder<TEntity> builder = new VerticalReportSchemaBuilder<TEntity>();
+            ReportVariableData[] properties = this.GetProperties<TEntity>();
 
             this.AddColumns(builder, properties);
             this.AddComplexHeader(builder, properties);
+
+            return builder;
         }
 
         private void AddColumns<TEntity>(VerticalReportSchemaBuilder<TEntity> builder, ReportVariableData[] properties)
@@ -76,24 +119,33 @@ namespace Reports.Extensions.Builders.BuilderHelpers
 
         private void AddColumn<TEntity>(VerticalReportSchemaBuilder<TEntity> builder, PropertyInfo property, ReportVariableAttribute attribute)
         {
-            ParameterExpression parameter = Expression.Parameter(typeof(TEntity), "x");
-            MemberExpression memberExpression = Expression.Property(parameter, typeof(TEntity), property.Name);
-            LambdaExpression lambdaExpression = Expression.Lambda(memberExpression, parameter);
-
-            IReportCellsProvider<TEntity> instance = (IReportCellsProvider<TEntity>) Activator.CreateInstance(
-                typeof(EntityPropertyReportCellsProvider<,>)
-                    .MakeGenericType(typeof(TEntity), property.PropertyType),
-                attribute.Title,
-                lambdaExpression.Compile()
-            );
+            IReportCellsProvider<TEntity> instance = this.CreateCellsProvider<TEntity>(property, attribute);
 
             builder.AddColumn(instance);
 
-            this.ApplyAttribute(instance, attribute);
+            this.ApplyAttribute(builder, attribute);
         }
 
-        private void ApplyAttribute<TEntity>(IReportCellsProvider<TEntity> provider, ReportVariableAttribute attribute)
+        private void ApplyAttribute<TEntity>(ReportSchemaBuilder<TEntity> builder, ReportVariableAttribute attribute)
         {
+        }
+
+        private ReportVariableData[] GetProperties<TEntity>()
+        {
+            return typeof(TEntity).GetProperties()
+                .Select(p => new
+                {
+                    Property = p,
+                    Attribute = p.GetCustomAttribute<ReportVariableAttribute>(),
+                })
+                .Where(x => x.Attribute != null)
+                .Select(x => new ReportVariableData()
+                {
+                    Property = x.Property,
+                    Attribute = x.Attribute,
+                })
+                .OrderBy(x => x.Attribute.Order)
+                .ToArray();
         }
 
         private class ReportVariableData
