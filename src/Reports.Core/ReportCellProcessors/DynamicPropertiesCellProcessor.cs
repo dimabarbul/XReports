d16211c1835fe4d3d112f258ac455920d0d2aa8d
Dmitriy Barbul <dimabarbul@gmail.com>
2020-12-12 13:29:36 +0200

    Add DynamicPropertiesCellProcessor

---
+++ src/Reports.Core/ReportCellProcessors/DynamicPropertiesCellProcessor.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using Reports.Core.Interfaces;
+using Reports.Core.Models;
+
+namespace Reports.Core.ReportCellProcessors
+{
+    public class DynamicPropertiesCellProcessor<TSourceEntity> : IReportCellProcessor<TSourceEntity>
+    {
+        private readonly Func<TSourceEntity, IEnumerable<ReportCellProperty>> propertySelector;
+
+        public DynamicPropertiesCellProcessor(Func<TSourceEntity, IEnumerable<ReportCellProperty>> propertySelector)
+        {
+            this.propertySelector = propertySelector;
+        }
+
+        public void Process(ReportCell cell, TSourceEntity entity)
+        {
+            foreach (ReportCellProperty property in this.propertySelector(entity))
+            {
+                cell.AddProperty(property);
+            }
+        }
+    }
+}
