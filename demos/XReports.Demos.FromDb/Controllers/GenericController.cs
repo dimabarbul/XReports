89bf98678192052571b18eda025ac643572744aa
Dmitriy Barbul <dimabarbul@gmail.com>
2020-12-26 09:48:40 +0200

    Add support for IDataReader source in vertical report

--- demos/XReports.Demos.FromDb/Controllers/GenericController.cs
+++ demos/XReports.Demos.FromDb/Controllers/GenericController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Data;
 using System.Threading.Tasks;
 using Dapper;
 using Microsoft.AspNetCore.Mvc;
@@ -24,11 +25,11 @@ namespace XReports.Demos.FromDb.Controllers
         public async Task<IActionResult> Index()
         {
             await using SqliteConnection connection = new SqliteConnection("Data Source=Test.db");
-            IEnumerable<dynamic> products = await connection.QueryAsync("SELECT * FROM Products");
-            VerticalReportSchemaBuilder<dynamic> builder = new VerticalReportSchemaBuilder<dynamic>();
-            builder.AddColumn("ID", x => x.Id);
-            builder.AddColumn("Title", x => x.Title);
-            IReportTable<HtmlReportCell> reportTable = this.htmlConverter.Convert(builder.BuildSchema().BuildReportTable(products));
+            IDataReader dataReader = await connection.ExecuteReaderAsync("SELECT Id, Title FROM Products", CommandBehavior.CloseConnection);
+            VerticalReportSchemaBuilder<IDataReader> builder = new VerticalReportSchemaBuilder<IDataReader>();
+            builder.AddColumn("ID", x => x.GetInt32(0));
+            builder.AddColumn("Title", x => x.GetString(1));
+            IReportTable<HtmlReportCell> reportTable = this.htmlConverter.Convert(builder.BuildSchema().BuildReportTable(dataReader));
             string tableHtml = await this.stringWriter.WriteToStringAsync(reportTable);
 
             return View(new IndexViewModel()
--- src/XReports.Core/Models/VerticalReportSchema.cs
+++ src/XReports.Core/Models/VerticalReportSchema.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using XReports.Interfaces;
 
@@ -17,12 +19,40 @@ namespace XReports.Models
             return table;
         }
 
+        public IReportTable<ReportCell> BuildReportTable(IDataReader dataReader)
+        {
+            ReportTable<ReportCell> table = new ReportTable<ReportCell>
+            {
+                HeaderRows = this.CreateComplexHeader(),
+                Rows = this.GetRows(dataReader),
+            };
+
+            return table;
+        }
+
         private IEnumerable<IEnumerable<ReportCell>> GetRows(IEnumerable<TSourceEntity> source)
         {
             return source
                 .Select(entity => this.CellsProviders
                     .Select(p => this.AddTableProperties(p.CreateCell(entity)))
                 );
+        }
+
+        private IEnumerable<IEnumerable<ReportCell>> GetRows(IDataReader dataReader)
+        {
+            if (!typeof(IDataReader).IsAssignableFrom(typeof(TSourceEntity)))
+            {
+                throw new InvalidOperationException($"Report schema should should be of IDataReader");
+            }
+
+            while (dataReader.Read())
+            {
+                yield return
+                    this.CellsProviders
+                        .Select(p => this.AddTableProperties(p.CreateCell((TSourceEntity) dataReader)));
+            }
+
+            dataReader.Close();
         }
     }
 }
