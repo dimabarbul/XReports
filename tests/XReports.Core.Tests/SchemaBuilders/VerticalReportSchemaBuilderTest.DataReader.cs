4bd4856db76638293b2256ff0cdfeed4540b8f06
Dmitriy Barbul <dimabarbul@gmail.com>
2022-12-22 10:58:17 +0200

    Use using statement instead of expression

--- tests/XReports.Core.Tests/SchemaBuilders/VerticalReportSchemaBuilderTest.DataReader.cs
+++ tests/XReports.Core.Tests/SchemaBuilders/VerticalReportSchemaBuilderTest.DataReader.cs
@@ -20,23 +20,25 @@ namespace XReports.Core.Tests.SchemaBuilders
             builder.AddColumn("Name", x => x.GetString(0));
             builder.AddColumn("Age", x => x.GetInt32(1));
 
-            using DataTable dataTable = new DataTable();
-            dataTable.Columns.AddRange(new[]
+            using (DataTable dataTable = new DataTable())
             {
-                new DataColumn("Name", typeof(string)),
-                new DataColumn("Age", typeof(int)),
-            });
-            dataTable.Rows.Add("John", 23);
-            dataTable.Rows.Add("Jane", 22);
+                dataTable.Columns.AddRange(new[]
+                {
+                    new DataColumn("Name", typeof(string)), new DataColumn("Age", typeof(int)),
+                });
+                dataTable.Rows.Add("John", 23);
+                dataTable.Rows.Add("Jane", 22);
 
-            using IDataReader dataReader = new DataTableReader(dataTable);
-            IReportTable<ReportCell> reportTable = builder.BuildSchema().BuildReportTable(dataReader);
+                using (IDataReader dataReader = new DataTableReader(dataTable))
+                {
+                    IReportTable<ReportCell> reportTable = builder.BuildSchema().BuildReportTable(dataReader);
 
-            reportTable.Rows.Should().BeEquivalentTo(new[]
-            {
-                new object[] { "John", 23 },
-                new object[] { "Jane", 22 },
-            });
+                    reportTable.Rows.Should().BeEquivalentTo(new[]
+                    {
+                        new object[] { "John", 23 }, new object[] { "Jane", 22 },
+                    });
+                }
+            }
         }
 
         [Fact]
@@ -46,18 +48,19 @@ namespace XReports.Core.Tests.SchemaBuilders
 
             builder.AddColumn("Value", s => s);
 
-            using DataTable dataTable = new DataTable();
-            dataTable.Columns.AddRange(new[]
+            using (DataTable dataTable = new DataTable())
             {
-                new DataColumn("Value", typeof(string)),
-            });
-            dataTable.Rows.Add("John");
-            dataTable.Rows.Add("Jane");
+                dataTable.Columns.AddRange(new[] { new DataColumn("Value", typeof(string)), });
+                dataTable.Rows.Add("John");
+                dataTable.Rows.Add("Jane");
 
-            using IDataReader dataReader = new DataTableReader(dataTable);
-            Action action = () => _ = builder.BuildSchema().BuildReportTable(dataReader);
+                using (IDataReader dataReader = new DataTableReader(dataTable))
+                {
+                    Action action = () => _ = builder.BuildSchema().BuildReportTable(dataReader);
 
-            action.Should().ThrowExactly<ArgumentException>();
+                    action.Should().ThrowExactly<ArgumentException>();
+                }
+            }
         }
 
         [Fact]
@@ -67,18 +70,19 @@ namespace XReports.Core.Tests.SchemaBuilders
 
             builder.AddColumn("Value", x => x.GetString(0));
 
-            using DataTable dataTable = new DataTable();
-            dataTable.Columns.AddRange(new[]
+            using (DataTable dataTable = new DataTable())
             {
-                new DataColumn("Value", typeof(string)),
-            });
-            dataTable.Rows.Add("John");
-            dataTable.Rows.Add("Jane");
+                dataTable.Columns.AddRange(new[] { new DataColumn("Value", typeof(string)), });
+                dataTable.Rows.Add("John");
+                dataTable.Rows.Add("Jane");
 
-            using IDataReader dataReader = new DataTableReader(dataTable);
-            Action action = () => _ = builder.BuildSchema().BuildReportTable(new[] { dataReader });
+                using (IDataReader dataReader = new DataTableReader(dataTable))
+                {
+                    Action action = () => _ = builder.BuildSchema().BuildReportTable(new[] { dataReader });
 
-            action.Should().ThrowExactly<ArgumentException>();
+                    action.Should().ThrowExactly<ArgumentException>();
+                }
+            }
         }
     }
 }
